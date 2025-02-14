a693dbc945e560e8715e438b2b3560ff250edb9c
love singh kalbhor <lovesinghkalbhor@gmail.com>
2025-02-14 23:04:36 +0530

    added ftp

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
+        version = 'NodeJS is woking ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
 });
