3b8662ab66f2f386962f1a4c0cf2a67f6ba306f1
love singh kalbhor <lovesinghkalbhor@gmail.com>
2025-02-19 16:37:57 +0530

    fixed download feature

--- dist/controllers/invoice.controllers.js
+++ dist/controllers/invoice.controllers.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.viewInvoice = void 0;
+exports.DownloadInvoice = exports.viewInvoice = void 0;
 const asyncHandler_1 = require("../utils/asyncHandler");
 const ApiResponse_1 = require("../utils/ApiResponse");
 const prismaObject_1 = __importDefault(require("../utils/prismaObject"));
@@ -51,4 +51,35 @@ const viewInvoice = (0, asyncHandler_1.asyncHandler)(async (req, res) => {
     });
 });
 exports.viewInvoice = viewInvoice;
+const DownloadInvoice = (0, asyncHandler_1.asyncHandler)(async (req, res) => {
+    const { receiptNo } = req.query;
+    // Validate the phone number
+    if (!receiptNo || typeof receiptNo !== "string") {
+        return res
+            .status(401)
+            .json(new ApiResponse_1.ApiResponse(401, {}, "Invoice number is required"));
+    }
+    // Query the database for the donor by phone number
+    const results = await prismaObject_1.default.donation.findFirst({
+        where: {
+            receiptNo: Number(receiptNo),
+            // Ensure the phone number is treated as a string
+        },
+        include: {
+            items: true,
+        },
+    });
+    if (!results) {
+        return res
+            .status(401)
+            .json(new ApiResponse_1.ApiResponse(401, {}, "Donor not found,can not generate reciept"));
+    }
+    res.render("downloadableInvoice", {
+        donation: results,
+        amountInWords: (0, helperFunction_1.numberToWords)(results?.amount || 0),
+        formattedName: formattedName(results?.authorizedPersonName || ""),
+        downloadUrl: process.env.DOWNLOAD_RECEIPT_URL,
+    });
+});
+exports.DownloadInvoice = DownloadInvoice;
 //# sourceMappingURL=invoice.controllers.js.map
--- dist/routes/invoice.route.js
+++ dist/routes/invoice.route.js
@@ -4,7 +4,9 @@ exports.invoiceRouter = void 0;
 const express_1 = require("express");
 const invoice_controllers_1 = require("../controllers/invoice.controllers");
 const auth_middleware_1 = require("../middleware/auth.middleware");
+const invoice_controllers_2 = require("../controllers/invoice.controllers");
 const invoiceRouter = (0, express_1.Router)();
 exports.invoiceRouter = invoiceRouter;
 invoiceRouter.route("/invoice").get(auth_middleware_1.verifyJWT, invoice_controllers_1.viewInvoice);
+invoiceRouter.route("/downloadInvoice").get(invoice_controllers_2.DownloadInvoice);
 //# sourceMappingURL=invoice.route.js.map
